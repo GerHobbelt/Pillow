264ab6f99532c5d7c2b78f005cc2e730df79a261
Frederick Price <rprice@pricemail.ca>
2024-03-18 15:16:54 -0400

    CVE-2020-10177 cherry-pick c66d8aa75436f334f686fe32bca8e414bcdd18e6

--- src/libImaging/FliDecode.c
+++ src/libImaging/FliDecode.c
@@ -248,9 +248,169 @@ int ImagingFliDecode(Imaging im, ImagingCodecState state, UINT8 *buf,
       state->errcode = IMAGING_CODEC_BROKEN;
       return -1;
     }
-    if (advance < 0 || advance > bytes) {
-      state->errcode = IMAGING_CODEC_OVERRUN;
-      return -1;
+
+    chunks = I16(ptr+6);
+    ptr += 16;
+    bytes -= 16;
+
+    /* Process subchunks */
+    for (c = 0; c < chunks; c++) {
+	UINT8* data;
+	if (bytes < 10) {
+	    state->errcode = IMAGING_CODEC_OVERRUN;
+	    return -1;
+	}
+	data = ptr + 6;
+	switch (I16(ptr+4)) {
+	case 4: case 11:
+	    /* FLI COLOR chunk */
+	    break; /* ignored; handled by Python code */
+	case 7:
+	    /* FLI SS2 chunk (word delta) */
+	    lines = I16(data); data += 2;
+	    for (l = y = 0; l < lines && y < state->ysize; l++, y++) {
+		UINT8* buf = (UINT8*) im->image[y];
+		int p, packets;
+		packets = I16(data); data += 2;
+		while (packets & 0x8000) {
+		    /* flag word */
+		    if (packets & 0x4000) {
+			y += 65536 - packets; /* skip lines */
+			if (y >= state->ysize) {
+			    state->errcode = IMAGING_CODEC_OVERRUN;
+			    return -1;
+			}
+			buf = (UINT8*) im->image[y];
+		    } else {
+			/* store last byte (used if line width is odd) */
+			buf[state->xsize-1] = (UINT8) packets;
+		    }
+		    packets = I16(data); data += 2;
+		}
+		for (p = x = 0; p < packets; p++) {
+		    x += data[0]; /* pixel skip */
+		    if (data[1] >= 128) {
+			i = 256-data[1]; /* run */
+			if (x + i + i > state->xsize)
+			    break;
+			for (j = 0; j < i; j++) {
+			    buf[x++] = data[2];
+			    buf[x++] = data[3];
+			}
+			data += 2 + 2;
+		    } else {
+			i = 2 * (int) data[1]; /* chunk */
+			if (x + i > state->xsize)
+			    break;
+			memcpy(buf + x, data + 2, i);
+			data += 2 + i;
+			x += i;
+		    }
+		}
+		if (p < packets)
+		    break; /* didn't process all packets */
+	    }
+	    if (l < lines) {
+		/* didn't process all lines */
+		state->errcode = IMAGING_CODEC_OVERRUN;
+		return -1;
+	    }
+	    break;
+	case 12:
+	    /* FLI LC chunk (byte delta) */
+	    y = I16(data); ymax = y + I16(data+2); data += 4;
+	    for (; y < ymax && y < state->ysize; y++) {
+		UINT8* out = (UINT8*) im->image[y];
+		int p, packets = *data++;
+		for (p = x = 0; p < packets; p++, x += i) {
+		    x += data[0]; /* skip pixels */
+		    if (data[1] & 0x80) {
+			i = 256-data[1]; /* run */
+			if (x + i > state->xsize)
+			    break;
+			memset(out + x, data[2], i);
+			data += 3;
+		    } else {
+			i = data[1]; /* chunk */
+			if (x + i > state->xsize)
+			    break;
+			memcpy(out + x, data + 2, i);
+			data += i + 2;
+		    }
+		}
+		if (p < packets)
+		    break; /* didn't process all packets */
+	    }
+	    if (y < ymax) {
+		/* didn't process all lines */
+		state->errcode = IMAGING_CODEC_OVERRUN;
+		return -1;
+	    }
+	    break;
+	case 13:
+	    /* FLI BLACK chunk */
+	    for (y = 0; y < state->ysize; y++)
+		memset(im->image[y], 0, state->xsize);
+	    break;
+	case 15:
+	    /* FLI BRUN chunk */
+	    /* data = ptr + 6 */
+	    for (y = 0; y < state->ysize; y++) {
+		UINT8* out = (UINT8*) im->image[y];
+		data += 1; /* ignore packetcount byte */
+		for (x = 0; x < state->xsize; x += i) {
+		    if (data + 2 > ptr + bytes ) {
+			/* Out of Bounds Read issue, guaranteed to try to read 2 from data */
+			state->errcode = IMAGING_CODEC_OVERRUN;
+			return -1;
+		    }
+		    if (data[0] & 0x80) {
+			i = 256 - data[0];
+			if (x + i > state->xsize) {
+			    break; /* safety first */
+			}
+			if (data + i + 1 > ptr + bytes ) {
+			    /* Out of Bounds Read issue */
+			    state->errcode = IMAGING_CODEC_OVERRUN;
+			    return -1;
+			}
+			memcpy(out + x, data + 1, i);
+			data += i + 1;
+		    } else {
+			i = data[0];
+			if (x + i > state->xsize)
+			    break; /* safety first */
+			memset(out + x, data[1], i);
+			data += 2;
+		    }
+		}
+		if (x != state->xsize) {
+		    /* didn't unpack whole line */
+		    state->errcode = IMAGING_CODEC_OVERRUN;
+		    return -1;
+		}
+	    }
+	    break;
+	case 16:
+	    /* COPY chunk */
+	    for (y = 0; y < state->ysize; y++) {
+		UINT8* buf = (UINT8*) im->image[y];
+		memcpy(buf, data, state->xsize);
+		data += state->xsize;
+	    }
+	    break;
+	case 18:
+	    /* PSTAMP chunk */
+	    break; /* ignored */
+	default:
+	    /* unknown chunk */
+	    /* printf("unknown FLI/FLC chunk: %d\n", I16(ptr+4)); */
+	    state->errcode = IMAGING_CODEC_UNKNOWN;
+	    return -1;
+	}
+	advance = I32(ptr);
+	ptr += advance;
+	bytes -= advance;
     }
     ptr += advance;
     bytes -= advance;
